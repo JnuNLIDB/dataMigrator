a60b8eb074c8d40387e9dd9fc55720e8edd2e6b4
Fox_white <vct.xie@gmail.com>
2023-05-24 11:40:21 +0800

    400 pool and updated structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(result_option_inspect)]
+
 mod schema;
 
 use futures::stream::FuturesUnordered;
@@ -7,48 +9,63 @@ use std::fmt::Write;
 use backoff::future::retry;
 use backoff::ExponentialBackoff;
 use sqlx::postgres::PgPoolOptions;
-use sqlx::{Error, Row};
+use sqlx::{Error, Pool, Postgres, Row};
 use std::fs::File;
+
 use std::io::BufReader;
 use std::path::Path;
 
 use crate::schema::Root;
 use dotenv::dotenv;
 use indicatif::{ProgressBar, ProgressState, ProgressStyle};
+use log::{error, info, warn};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     dotenv().ok();
+    env_logger::builder().filter_level(log::LevelFilter::Warn).init();
+
     let url = std::env::var("POSTGRES_URL").expect("POSTGRES_URL must be set.");
     let pool = PgPoolOptions::new()
         .max_connections(400)
         .connect(&url)
         .await?;
-
     sqlx::query(
-        "CREATE TABLE IF NOT EXISTS authors (
+        "CREATE TABLE IF NOT EXISTS country (
         id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         name TEXT UNIQUE NOT NULL,
         geography TEXT,
+        belt_and_road BOOLEAN
+    )",
+    ).execute(&pool).await?;
+
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS people (
+        id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        name TEXT UNIQUE NOT NULL,
+        country_id INT,
         title TEXT,
+        origin TEXT,
         identity TEXT,
-        \"from\" TEXT,
-        orob TEXT,
-        orob_region TEXT
+        CONSTRAINT fk_country
+            FOREIGN KEY (country_id)
+            REFERENCES country(id)
+            ON DELETE CASCADE
     );",
     )
+
     .execute(&pool)
     .await?;
     sqlx::query(
         "CREATE TABLE IF NOT EXISTS source (
         id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         name TEXT UNIQUE NOT NULL,
-        geography TEXT,
-        geopolitical TEXT,
-        orob TEXT,
-        orob_region TEXT,
-        country TEXT,
-        \"from\" TEXT
+        country_id INT,
+        origin TEXT,
+        CONSTRAINT fk_country
+            FOREIGN KEY (country_id)
+            REFERENCES country(id)
+            ON DELETE CASCADE
     );",
     )
     .execute(&pool)
@@ -66,6 +83,7 @@ async fn main() -> Result<(), Error> {
         "CREATE TABLE IF NOT EXISTS source_article (\
         source_id INT NOT NULL,
         article_id INT NOT NULL,
+        UNIQUE (source_id, article_id),
         CONSTRAINT fk_source
             FOREIGN KEY (source_id)
             REFERENCES source(id)
@@ -82,13 +100,11 @@ async fn main() -> Result<(), Error> {
         "CREATE TABLE IF NOT EXISTS opinion (
         id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         author_id INT NOT NULL,
-        start INT NOT NULL,
-        \"end\" INT NOT NULL,
         text TEXT UNIQUE NOT NULL,
         article_id INT NOT NULL,
         CONSTRAINT fk_author
             FOREIGN KEY (author_id)
-            REFERENCES authors(id)
+            REFERENCES people(id)
             ON DELETE CASCADE,
         CONSTRAINT fk_article
             FOREIGN KEY (article_id)
@@ -99,11 +115,31 @@ async fn main() -> Result<(), Error> {
     .execute(&pool)
     .await?;
 
-    let path = Path::new("./report_2023_new.json");
-    let file = File::open(path).unwrap();
-    let reader = BufReader::new(file);
-    let roots = serde_json::from_reader::<BufReader<File>, Vec<Root>>(reader).unwrap();
+    // Iterate over file in data folder
+    warn!("Start processing files in data folder");
+    let data_dir = Path::new("./data_new");
+    let iter = data_dir
+        .read_dir()
+        .unwrap()
+        .filter_map(|entry| {
+            let entry = entry.unwrap();
+            if entry.file_type().unwrap().is_file() {
+                Some(entry.path())
+            } else {
+                None
+            }
+        });
+    for path in iter {
+        let file = File::open(&path).unwrap();
+        let reader = BufReader::new(file);
+        warn!("Start processing file: {}", path.to_str().unwrap());
+        let roots = serde_json::from_reader::<BufReader<File>, Vec<Root>>(reader).unwrap();
+        process_roots(&pool, roots).await.unwrap();
+    }
+    Ok(())
+}
 
+async fn process_roots(pool: &Pool<Postgres>, roots: Vec<Root>) -> Result<(), Error> {
     let mut futs = FuturesUnordered::new();
     let pb = ProgressBar::new(roots.len() as u64);
     pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
@@ -113,33 +149,56 @@ async fn main() -> Result<(), Error> {
 
     for x in roots {
         let pool = pool.clone();
+        let pb = pb.clone();
         let fut = async move {
             let mut source_ids = Vec::with_capacity(x.source.len());
             for source in x.source {
                 if source.name.is_none() {
                     continue;
                 }
+                let country_id = match source.country {
+                    Some(_) => Some(
+                        retry(ExponentialBackoff::default(), || async {
+                            Ok(
+                                match sqlx::query(
+                                    "INSERT INTO country (name, geography, belt_and_road) \
+                                        VALUES ($1, $2, $3) ON CONFLICT (name) DO NOTHING RETURNING id",
+                                )
+                                    .bind(&source.country)
+                                    .bind(&source.geography)
+                                    .bind(&source.orob.is_some())
+                                    .fetch_one(&pool)
+                                    .await
+                                {
+                                    Ok(row) => row.get::<i32, _>("id"),
+                                    Err(e1) => sqlx::query("SELECT id FROM country WHERE name = $1")
+                                        .bind(&source.country)
+                                        .fetch_one(&pool)
+                                        .await.inspect_err(|e2| error!("1. {:?}, 2. {:?}", e1, e2))?
+                                        .get::<i32, _>("id"),
+                                }
+                            )
+                        }).await?
+                    ),
+                    None => None,
+                };
                 let id = retry(ExponentialBackoff::default(), || async {
                     Ok(
                         match sqlx::query(
-                            "INSERT INTO source (name, geography, country, geopolitical, orob, orob_region, \"from\") \
-                        VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (name) DO NOTHING RETURNING id",
+                            "INSERT INTO source (name, country_id, origin) \
+                            VALUES ($1, $2, $3) ON CONFLICT (name) DO NOTHING RETURNING id",
                         )
                             .bind(&source.name)
-                            .bind(&source.geography)
-                            .bind(&source.country)
-                            .bind(&source.geography)
-                            .bind(&source.orob)
-                            .bind(&source.orob_region)
+                            .bind(country_id)
                             .bind(&source.get_from())
                             .fetch_one(&pool)
                             .await
                         {
                             Ok(row) => row.get::<i32, _>("id"),
-                            Err(_e) => sqlx::query("SELECT id FROM source WHERE name = $1")
+                            Err(e1) => sqlx::query("SELECT id FROM source WHERE name = $1")
                                 .bind(&source.name)
                                 .fetch_one(&pool)
-                                .await?
+                                .await.inspect_err(|e2| error!("1. {:?}, 2. {:?}", e1, e2))?
                                 .get::<i32, _>("id"),
                         }
                     )
@@ -158,43 +217,69 @@ async fn main() -> Result<(), Error> {
                         "INSERT INTO article (title, time) \
                         VALUES ($1, $2) ON CONFLICT (title) DO NOTHING RETURNING id",
                     )
-                    .bind(&x.headline)
-                    .bind(update_time)
-                    .fetch_one(&pool)
-                    .await
-                    {
-                        Ok(row) => row.get::<i32, _>("id"),
-                        Err(_e) => sqlx::query("SELECT id FROM article WHERE title = $1")
-                            .bind(&x.headline)
-                            .fetch_one(&pool)
-                            .await?
-                            .get::<i32, _>("id"),
-                    },
-                )
-            })
-            .await?;
-
-            let author_id = retry(ExponentialBackoff::default(), || async {
-                Ok(
-                    match sqlx::query(
-                        "INSERT INTO authors (name, title, geography, identity, \"from\", orob, orob_region) \
-                        VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (name) DO NOTHING RETURNING id",
-                    )
-                        .bind(&x.people.name)
-                        .bind(&x.people.title)
-                        .bind(&x.people.geography)
-                        .bind(&x.people.get_identity())
-                        .bind(&x.people.get_from())
-                        .bind(&x.people.orob)
-                        .bind(&x.people.orob_region)
+                        .bind(&x.headline)
+                        .bind(update_time)
                         .fetch_one(&pool)
                         .await
                     {
                         Ok(row) => row.get::<i32, _>("id"),
-                        Err(_e) => sqlx::query("SELECT id FROM authors WHERE name = $1")
+                        Err(e1) => sqlx::query("SELECT id FROM article WHERE title = $1")
+                            .bind(&x.headline)
+                            .fetch_one(&pool)
+                            .await.inspect_err(|e2| pb.suspend(|| error!("1. {:?}, 2. {:?}", e1, e2)))?
+                            .get::<i32, _>("id"),
+                    },
+                )
+            })
+                .await?;
+
+            let people_country_id = match x.people.country {
+                Some(_) => Some(
+                    retry(ExponentialBackoff::default(), || async {
+                        Ok(
+                            match sqlx::query(
+                                "INSERT INTO country (name, geography, belt_and_road) \
+                                    VALUES ($1, $2, $3) ON CONFLICT (name) DO NOTHING RETURNING id",
+                            )
+                                .bind(&x.people.country)
+                                .bind(&x.people.geography)
+                                .bind(&x.people.orob.is_some())
+                                .fetch_one(&pool)
+                                .await
+                            {
+                                Ok(row) => row.get::<i32, _>("id"),
+                                Err(e1) => sqlx::query("SELECT id FROM country WHERE name = $1")
+                                    .bind(&x.people.country)
+                                    .fetch_one(&pool)
+                                    .await.inspect_err(|e2| error!("1. {:?}, 2. {:?}", e1, e2))?
+                                    .get::<i32, _>("id"),
+                            }
+                        )
+                    })
+                        .await?,
+                ),
+                None => None,
+            };
+
+            let author_id = retry(ExponentialBackoff::default(), || async {
+                Ok(
+                    match sqlx::query(
+                        "INSERT INTO people (name, country_id, origin, title, identity) \
+                        VALUES ($1, $2, $3, $4, $5) ON CONFLICT (name) DO NOTHING RETURNING id",
+                    )
+                        .bind(&x.people.name)
+                        .bind(people_country_id)
+                        .bind(&x.people.get_from())
+                        .bind(&x.people.title)
+                        .bind(&x.people.get_identity())
+                        .fetch_one(&pool)
+                        .await
+                    {
+                        Ok(row) => row.get::<i32, _>("id"),
+                        Err(e1) => sqlx::query("SELECT id FROM people WHERE name = $1")
                             .bind(&x.people.name)
                             .fetch_one(&pool)
-                            .await?
+                            .await.inspect_err(|e2| pb.suspend(|| error!("1. {:?}, 2. {:?}", e1, e2)))?
                             .get::<i32, _>("id"),
                     }
                 )
@@ -206,34 +291,44 @@ async fn main() -> Result<(), Error> {
                         "INSERT INTO source_article (source_id, article_id) \
                     VALUES ($1, $2) ON CONFLICT DO NOTHING",
                     )
-                    .bind(&source_id)
-                    .bind(&article_id)
-                    .execute(&pool)
-                    .await?)
+                        .bind(&source_id)
+                        .bind(&article_id)
+                        .execute(&pool)
+                        .await.inspect_err(|e| pb.suspend(|| error!("1. {:?}", e)))?)
                 })
-                .await?;
+                    .await?;
             }
+
             for op in x.people.opinion {
                 retry(ExponentialBackoff::default(), || async {
-                    Ok(sqlx::query(
-                        "INSERT INTO opinion (author_id, score, start, \"end\", text, article_id) \
-                                VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT DO NOTHING",
+                    let result = sqlx::query(
+                        "INSERT INTO opinion (author_id, text, article_id) \
+                                VALUES ($1, $2, $3) ON CONFLICT DO NOTHING",
                     )
-                    .bind(&author_id)
-                    .bind(&op.score)
-                    .bind(&op.start)
-                    .bind(&op.end)
-                    .bind(&op.text)
-                    .bind(&article_id)
-                    .execute(&pool)
-                    .await?)
+                        .bind(&author_id)
+                        .bind(&op.text)
+                        .bind(&article_id)
+                        .execute(&pool)
+                        .await;
+                    Ok(
+                        match result {
+                            Err(e) => {
+                                let error = e.as_database_error().unwrap();
+                                match error.code().unwrap().as_ref() {
+                                    "54000" => Ok(()),
+                                    _ => Err(e),
+                                }
+                            }
+                            Ok(_) => Ok(()),
+                        }.inspect_err(|e| pb.suspend(|| error!("1. {:?}", e)))?,
+                    )
                 })
-                .await?;
+                    .await?;
             }
+            pb.inc(1);
             Ok::<(), Error>(())
         };
         futs.push(fut);
-        pb.inc(1);
         if futs.len() >= 400 {
             futs.next().await.unwrap().unwrap();
         }
--- tmp.py
+++ tmp.py
@@ -1,18 +1,31 @@
+import os
+import logging
+
 import orjson as json
 
-print("Parsing Json")
-data: list = json.loads(open('report_2023.json', encoding='utf-8').read())
-k = 0
-new_data = []
-for i, n in enumerate(data):
-    if "Source" not in n or any(("Id_" not in k for k in n["Source"])):
-        continue
-    if "Opinion" not in n["People"] or type(n["People"]["Opinion"]) is dict:
-        n["People"]["Opinion"] = [n["People"]["Opinion"]]
-    new_data.append(n)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-with open("report_2023_new.json", "wb") as f:
-    f.write(json.dumps(new_data))
+for file in os.listdir("data"):
+    new_data = []
+
+    logger.info("Open file: ", file)
+    j = open("data/" + file, encoding='utf-8').read()
+    j = "[" + j.replace("\n", ",").rstrip(",") + "]" if j.count("\n") > 0 else j
+    logger.info("Load json")
+    data: list = json.loads(j)
+    for i, n in enumerate(data):
+        if "Source" not in n or any(("Id_" not in k for k in n["Source"])):
+            continue
+        if "Opinion" not in n["People"] or type(n["People"]["Opinion"]) is dict:
+            n["People"]["Opinion"] = [n["People"]["Opinion"]]
+        new_data.append(n)
+    logger.info("Total: ", len(new_data))
+
+    new_file_name = file.replace(".json", "_new.json")
+
+    with open(new_file_name, "wb") as f:
+        f.write(json.dumps(new_data))
 #
 # with open('report_2021_new.json', encoding='utf-8') as f:
 #     string = f.read()
